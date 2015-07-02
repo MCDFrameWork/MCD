09a85582dd42ec3e77df3a1527c34df775a6743e
fengxiao <heke@fengxiao.lan>
2015-07-03 00:18:21 +0800

    Initial Commit

---
+++ MCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCD
+//
+//  Created by fengxiao on 15/7/3.
+//  Copyright (c) 2015年 UniFrameWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCD
+//
+//  Created by fengxiao on 15/7/3.
+//  Copyright (c) 2015年 UniFrameWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
